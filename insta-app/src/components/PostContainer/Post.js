aec8b480d53c5b8812e3d98582c9a97ea6bde530
Sydney Crumley <srsimps19@gmail.com>
2019-05-06 16:17:31 -0600

    styled post container

--- insta-app/src/components/PostContainer/Post.js
+++ insta-app/src/components/PostContainer/Post.js
@@ -2,20 +2,28 @@ import React from "react";
 import CommentSection from '../CommentSection/CommentSection.js';
 import moment from 'moment';
 import PropTypes from 'prop-types';
+import './PostContainer.scss'
 
 function Post(props) {
     return (
-        <div>
+        <>
             {props.data.map(data => (
-                <div key={data.imageUrl} className="posts">
-                    <img src={data.thumbnailUrl} alt={data.username}/>
-                    <h4>{data.username}</h4>
+                <div key={data.imageUrl} className="post">
+                    <div className="postHeader">
+                        <img src={data.thumbnailUrl} alt={data.username}/>
+                        <h4>{data.username}</h4>
+                    </div>
                     <img src={data.imageUrl} alt="from post"/>
-                    <CommentSection comments={data.comments}/>
-                    <p>{moment().startOf(data.timestamp).fromNow()}</p>
+                    <div className="belowPicture">
+                        <i className="far fa-heart fa-2x"></i>
+                        <i className="far fa-comment fa-2x"></i>
+                        <p>{data.likes} likes</p>
+                        <CommentSection comments={data.comments}/>
+                        <p className="timestamp">{moment().startOf(data.timestamp).fromNow()}</p>
+                    </div>
                 </div>
             ))} 
-        </div>
+        </>
     )
 }
 
